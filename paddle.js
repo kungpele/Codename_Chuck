3777f59ccc4ce73e1e96dd354763d2e196c75fe8
kungpele <per.brobeck@protonmail.com>
2023-02-24 08:16:18 -0600

    Working

---
+++ paddle.js
@@ -0,0 +1,38 @@
+
+
+
+const SPEED = 0.02
+
+export default class Paddle {
+    constructor(paddleElem){
+        this.paddleElem = paddleElem
+        this.reset()
+    }
+
+    get position() {
+        return parseFloat(
+            getComputedStyle(this.paddleElem).getPropertyValue("--position"))
+    }
+
+    set position(value)  {
+    return this.paddleElem.style.setProperty("--position", value)
+    }
+
+
+    update(delta, ballHeight) {
+        this.position += SPEED * delta * (ballHeight - this.position)
+
+    }
+
+    rect() {
+        return this.paddleElem.getBoundingClientRect()
+    }
+
+    reset(){
+        this.position = 50
+    }
+
+
+
+
+}
--- scripts.js
+++ scripts.js
@@ -1,20 +1,65 @@
 // Update loop
 
-import ball from "./Ball.js"
+import Ball from "./Ball.js"
+import Paddle from "./paddle.js"
 
 const ball = new Ball(document.getElementById("ball"))
+const playerPaddle = new Paddle(document.getElementById("player_paddle"))
+const aiPaddle = new Paddle(document.getElementById("AI_paddle"))
+const playerScoreElem = document.getElementById("playerscore")
+const AIScoreElem = document.getElementById("AIscore")
+
+const ScoreBoard = document.getElementById("scoreboard")
+const GameBoard = document.getElementById("gameboard")
+
+
+
+
+
 
 let lastTime
 function update(time)
 {
     if(lastTime != null) {
         const delta = time - lastTime
-        //Update code
-        ball.update(delta)
-        console.log(delta)
-    }
- lastTime = time;
- window.requestAnimationFrame(update)
+        //Update code 
+        ball.update(delta, [playerPaddle.rect(), aiPaddle.rect()])
+        aiPaddle.update(delta, ball.y)
+        const hue = parseFloat(getComputedStyle(document.documentElement).getPropertyValue
+        ("--hue"))
+
+        document.documentElement.style.setProperty("--hue", hue+delta*0.01)
+
+        if (isLose()){
+             handleLose() 
+        }
+    } 
+    lastTime = time;
+    window.requestAnimationFrame(update)
 }
+
+function isLose() {
+  const rect = ball.rect()
+    return rect.right >= window.innerWidth || rect.left <= 0
+
+}
+
+function handleLose() {
+    const rect = ball.rect()
+    if (rect.right >= window.innerWidth){
+        playerScoreElem.textContent = parseInt(playerScoreElem.textContent) + 1
+    }
+    else {
+            AIScoreElem.textContent = parseInt(AIScoreElem.textContent) + 1
+        }
+    
+    ball.reset()
+    aiPaddle.reset()
+}
+
+
+document.addEventListener("mousemove", e => {
+    playerPaddle.position = (e.y / window.innerHeight) *100 
+})
 
 window.requestAnimationFrame(update) 
